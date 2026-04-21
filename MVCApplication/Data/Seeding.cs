39b70858ee16b929dc307a5c7dc0ebd70476c080
IttyBittyAC <elijah.j.speering@gmail.com>
2026-04-21 16:31:38 +0800

    Added a proper hashed password instead of a placeholder to the SeedAdminUser in seeding, replaced it with the default password of admin123

--- MVCApplication/Data/Seeding.cs
+++ MVCApplication/Data/Seeding.cs
@@ -15,7 +15,9 @@ namespace MVCApplication.Data
             var exist = await s.ExecuteScalarAsync<int>("select count(1) from users where role = 'admin'");
             if (exist > 0) return;
 
-            var id = await s.ExecuteScalarAsync<int>(@"insert into users(email, password_hash, username, fullname, role) values(@email, @hash, @username, @fullname, 'admin')  returning id",  new { email = "admin@example.com", hash = "hashed_password_here", username = "admin",fullname = "Admin User" });
+            // hashing the password should be done properly in a real application, this is just a placeholder
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword("admin123", workFactor: 12);
+            var id = await s.ExecuteScalarAsync<int>(@"insert into users(email, password_hash, username, fullname, role) values(@email, @hash, @username, @fullname, 'admin')  returning id",  new { email = "admin@example.com", hash = hashedPassword, username = "admin",fullname = "Admin User" });
         }
     }
 }
